3a533467091415de7db1ae125f9587f247a0d6fd
smolinskiJP <104567183+smolinskiJP@users.noreply.github.com>
2023-03-07 09:03:31 -0300

    Update Clock.java
    Polimento do print (h:m:s)

--- Clock/Clock.java
+++ Clock/Clock.java
@@ -2,18 +2,27 @@ package Java.Clock;
 
 public class Clock extends Thread{
 
-    private int sec = 0;
+    private int seg = 0, min = 0, hor = 0;
 
     @Override
     public void run(){
         while(true){
-            sec++;
-            System.out.println(sec + " segundos");
+            seg++;
+            if (seg >= 60){
+                min++;
+                seg = 0;
+            }
+            if (min >= 60){
+                hor++;
+                min = 0;
+            }
             try{
                 sleep(1000);
             } catch (InterruptedException e){
                 e.printStackTrace();
             }
+
+            System.out.println(hor + ":" + min + ":" + seg);
         }
     }
 }
